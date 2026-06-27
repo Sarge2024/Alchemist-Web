a3adca9ccbfa271771559b7ef72188228a4cbacf
Sergio Stulzer <sagacitas.assessoria@gmail.com>
2026-06-27 19:12:00 -0300

    chore: update index.html revision in service worker cache manifest

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -78,7 +78,7 @@ define(['./workbox-7e5eb42b'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.pli7hje4124"
+    "revision": "0.do3mbrnh9lg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
