5662d40a438f06ec1d275a1fc555e21eb6c0af78
Sergio Stulzer <sagacitas.assessoria@gmail.com>
2026-07-17 11:00:09 -0300

    chore: remove obsolete test, update, and data migration scripts

--- test-categories.js
+++
@@ -1,8 +0,0 @@
-const fetch = require('node-fetch');
-
-async function test() {
-  const res = await fetch('http://localhost:3000/api/categories');
-  const data = await res.json();
-  console.log("Categories response:", data);
-}
-test();
